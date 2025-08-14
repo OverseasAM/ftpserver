98a45c361afd4327ee25c88e2be24ebe900b7a75
Benjamin Michel <benjamin.michel@overseasam.com>
2025-08-14 17:58:51 +0200

    Added SSH key authentication method

--- fs/sftp/sftp.go
+++ fs/sftp/sftp.go
@@ -4,6 +4,8 @@ package sftp
 import (
 	"fmt"
 	"net"
+	"os"
+	"strings"
 
 	"github.com/pkg/sftp"
 	"github.com/spf13/afero"
@@ -28,12 +30,72 @@ func LoadFs(access *confpar.Access) (afero.Fs, error) {
 	par := access.Params
 	config := &ssh.ClientConfig{
 		User: par["username"],
-		Auth: []ssh.AuthMethod{
+	}
+
+	// Load host key if provided
+	hostKeyPath, hasHostKey := par["hostKey"]
+	var hostKey ssh.PublicKey
+	if hasHostKey && hostKeyPath != "" {
+		hostKeyBytes, err := os.ReadFile(hostKeyPath)
+		if err != nil {
+			return nil, &ConnectionError{Source: fmt.Errorf("unable to read host key: %w", err)}
+		}
+		hostKey, _, _, _, err = ssh.ParseAuthorizedKey(hostKeyBytes)
+		if err != nil {
+			return nil, &ConnectionError{Source: fmt.Errorf("unable to parse host key: %w", err)}
+		}
+	}
+
+	// Load authmethod if provided
+	authMethod, hasAuthMethod := par["method"]
+	if hasAuthMethod && authMethod != "" {
+		authMethod = strings.ToLower(authMethod)
+	} else {
+		authMethod = "password"
+	}
+
+	switch authMethod {
+	case "publickey":
+		key, err := os.ReadFile(par["privateKey"])
+		if err != nil {
+			return nil, &ConnectionError{Source: fmt.Errorf("unable to read private key: %w", err)}
+		}
+		var signer ssh.Signer
+		passphrase, hasPassphrase := par["privateKeyPassphrase"]
+		if hasPassphrase && passphrase != "" {
+			signer, err = ssh.ParsePrivateKeyWithPassphrase(key, []byte(passphrase))
+			if err != nil {
+				return nil, &ConnectionError{Source: fmt.Errorf("unable to parse private key with passphrase: %w", err)}
+			}
+		} else {
+			signer, err = ssh.ParsePrivateKey(key)
+			if err != nil {
+				return nil, &ConnectionError{Source: fmt.Errorf("unable to parse private key: %w", err)}
+			}
+		}
+		config.Auth = []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		}
+		if hostKey != nil {
+			config.HostKeyCallback = ssh.FixedHostKey(hostKey)
+		} else {
+			config.HostKeyCallback = func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+				return nil
+			}
+		}
+	case "password", "":
+		config.Auth = []ssh.AuthMethod{
 			ssh.Password(par["password"]),
-		},
-		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-			return nil
-		},
+		}
+		if hostKey != nil {
+			config.HostKeyCallback = ssh.FixedHostKey(hostKey)
+		} else {
+			config.HostKeyCallback = func(hostname string, remote net.Addr, key ssh.PublicKey) error {
+				return nil
+			}
+		}
+	default:
+		fmt.Println("SFTP auth method not set")
 	}
 
 	// Dial your ssh server.
@@ -47,5 +109,22 @@ func LoadFs(access *confpar.Access) (afero.Fs, error) {
 		return nil, &ConnectionError{Source: errSftp}
 	}
 
+	// Get the SFTP user's home directory
+	/*
+		homeDir, err := client.Getwd()
+		if err != nil {
+			return nil, &ConnectionError{Source: fmt.Errorf("unable to get SFTP home directory: %w", err)}
+		}
+
+		//fmt.Println("SFTP user's home directory:", homeDir)
+	*/
+
+	basePath, hasBasePath := par["basePath"]
+	if hasBasePath && basePath != "" {
+		fs := afero.NewBasePathFs(sftpfs.New(client), basePath)
+		return fs, nil
+	}
+
 	return sftpfs.New(client), nil
+
 }
